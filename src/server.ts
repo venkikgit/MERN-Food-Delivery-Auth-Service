3b48e9034af7973b6ac3d1c1c71178367c8fe6a4
Venkatesh Kolagani <venkateswarlukolagani@Venkis-MacBook-Pro.local>
2023-11-03 08:44:32 +0530

    test husky6

--- src/server.ts
+++ src/server.ts
@@ -2,3 +2,16 @@
 
 // console.log('PORT', Config.PORT);
 // console.log('PORT', Config.NODE_ENV);
+
+// console.log("Server is running");
+
+function login() {
+    const user = {
+        name: 'venki',
+        id: '1',
+    };
+
+    const fullName = user.name;
+    return user.name + fullName;
+}
+login();
