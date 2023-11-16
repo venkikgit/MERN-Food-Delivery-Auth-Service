e1b13a8a91fac1b3c45186f5cfaac49d287977c9
venkikgit <venki.apiiit@gmail.com>
2023-11-16 17:23:13 +0530

    setup database

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,18 @@
 import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({
+    path: path.join(__dirname, '../../.env.${process.env.NODE_ENV}'),
+});
 
-dotenv.config({ path: './.env' });
-
-const { PORT, NODE_ENV } = process.env;
+const { PORT, NODE_ENV, DB_HOST, DB_PORT, DB_USERNAME, DB_PASSWORD, DB_NAME } =
+    process.env;
 
 export const Config = {
     PORT,
     NODE_ENV,
+    DB_HOST,
+    DB_PORT,
+    DB_USERNAME,
+    DB_PASSWORD,
+    DB_NAME,
 };
